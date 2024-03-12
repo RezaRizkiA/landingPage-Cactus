68336bddf652d3a2f9625b7cbbcc038fdf48d10a
Reza Rizki Akbar <reza@Rezas-MacBook-Pro.local>
2024-03-13 00:11:35 +0700

    created: scrollbar, scrollup, section-active

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,40 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== ADD BLUR HEADER ===============*/
 const blurHeader = () => {
-  const header = document.getElementById('header')
-  this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 /*=============== SHOW SCROLL UP ===============*/
-
+const showScrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scrollup")
+    : scrollUp.classList.remove("show-scrollup");
+};
+window.addEventListener("scroll", showScrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const sectionActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("section-active");
+    } else {
+      sectionClass.classList.remove("section-active");
+    }
+  });
+};
+window.addEventListener('scroll', sectionActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
